797b620fd362621077bf5aa3c67b782078d11b79
Lee Lounsbury <lee-lounsbury@uiowa.edu>
2020-01-28 15:35:45 -0800

    working for 1 gate

---
+++ GPS_test/GPS_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPS_test
+//
+//  Created by Lee Lounsbury on 1/25/20.
+//  Copyright © 2020 Lee Lounsbury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
